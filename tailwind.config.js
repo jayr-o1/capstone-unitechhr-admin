c6c3f80942ce1bed852c535ba7231a9e95882f2f
Jay-r Olores <jayrmalazarte.olores@gmail.com>
2025-01-29 12:16:59 +0800

    will fix green ping on notification

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+        // Ensure `animation` and `keyframes` are enabled
+        animation: true,
+    },
+};
