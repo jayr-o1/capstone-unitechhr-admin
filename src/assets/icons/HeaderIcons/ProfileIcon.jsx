c6c01e7718e03f90cdcfb19f51560f33c8c17941
Jay-r Olores <jayrmalazarte.olores@gmail.com>
2025-02-05 23:17:29 +0800

    will fix dashboard tomorrow

--- src/assets/icons/HeaderIcons/ProfileIcon.jsx
+++ src/assets/icons/HeaderIcons/ProfileIcon.jsx
@@ -1,7 +1,7 @@
 const ProfileIcon = () => (
     <svg
-        width="18"
-        height="18"
+        width="20"
+        height="20"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
--- src/components/Layouts/Sidebar.jsx
+++ src/components/Layouts/Sidebar.jsx
@@ -198,8 +198,8 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
                             className={`m-auto h-5 w-5 transition-transform duration-300 ${
                                 isSidebarOpen ? "rotate-180" : ""
                             }`}
-                            width="20"
-                            height="20"
+                            width="24"
+                            height="24"
                             viewBox="0 0 24 24"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
